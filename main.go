717c26d324289ba6f72c3a510254018ec089db3a
Jon Dewi Jones <jondewijones@gmail.com>
2019-12-09 19:03:28 +0000

    Update logging package

--- main.go
+++ main.go
@@ -4,7 +4,8 @@ import (
 	"os"
 
 	"github.com/ONSdigital/dp-publishing-dataset-controller/config"
-	"github.com/ONSdigital/go-ns/log"
+	"github.com/ONSdigital/dp-publishing-dataset-controller/routes"
+	"github.com/ONSdigital/log.go/log"
 	"github.com/ONSdigital/go-ns/server"
 	"github.com/gorilla/mux"
 )
@@ -12,19 +13,21 @@ import (
 func main() {
 	cfg, err := config.Get()
 	if err != nil {
-		log.Error(err, nil)
+		log.Event(nil, "error getting configuration", log.Error(err))
 		os.Exit(1)
 	}
 
 	log.Namespace = "dp-publishing-dataset-controller"
-	log.Info("config on startup", log.Data{"config": cfg})
+	log.Event(nil, "config on startup", log.Data{"config": cfg})
 
 	router := mux.NewRouter()
+
+	routes.Init(router, cfg)
 
 	s := server.New(cfg.BindAddr, router)
 
 	if err := s.ListenAndServe(); err != nil {
-		log.Error(err, nil)
+		log.Event(nil, "error starting http server", log.Error(err))
 		os.Exit(2)
 	}
 }
