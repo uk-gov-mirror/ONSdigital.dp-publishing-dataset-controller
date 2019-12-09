326b1adfc21907044b1f9157ef8038c6ac99333e
Jon Dewi Jones <jondewijones@gmail.com>
2019-12-09 15:28:28 +0000

    Add router and server

--- main.go
+++ main.go
@@ -5,16 +5,26 @@ import (
 
 	"github.com/ONSdigital/dp-publishing-dataset-controller/config"
 	"github.com/ONSdigital/go-ns/log"
+	"github.com/ONSdigital/go-ns/server"
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	log.Namespace = "dp-publishing-dataset-controller"
-
 	cfg, err := config.Get()
 	if err != nil {
 		log.Error(err, nil)
 		os.Exit(1)
 	}
 
+	log.Namespace = "dp-publishing-dataset-controller"
 	log.Info("config on startup", log.Data{"config": cfg})
+
+	router := mux.NewRouter()
+
+	s := server.New(cfg.BindAddr, router)
+
+	if err := s.ListenAndServe(); err != nil {
+		log.Error(err, nil)
+		os.Exit(2)
+	}
 }
