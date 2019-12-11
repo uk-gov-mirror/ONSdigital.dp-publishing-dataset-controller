6d82ef152b111dd01924ec794894ceeabf7df606
Jon Dewi Jones <jondewijones@gmail.com>
2019-12-11 10:24:17 +0000

    Update to handle clients explicitly rather than in a struct

--- routes/routes.go
+++ routes/routes.go
@@ -1,14 +1,15 @@
 package routes
 
 import (
+	"github.com/ONSdigital/dp-api-clients-go/dataset"
+	zebedee "github.com/ONSdigital/dp-api-clients-go/zebedee"
 	"github.com/ONSdigital/dp-publishing-dataset-controller/config"
 	"github.com/ONSdigital/dp-publishing-dataset-controller/handler"
-	"github.com/ONSdigital/dp-publishing-dataset-controller/model"
 	"github.com/gorilla/mux"
 )
 
 // Init initialises routes for the service
-func Init(router *mux.Router, cfg *config.Config, cli model.Clients) {
-	router.StrictSlash(true).Path("/datasets").HandlerFunc(handler.GetAllDatasets(cli.Dc))
-	router.StrictSlash(true).Path("/datasets/{datasetID}").HandlerFunc(handler.GetDataset(cli.Dc))
+func Init(router *mux.Router, cfg *config.Config, dc *dataset.Client, zc *zebedee.ZebedeeClient) {
+	router.StrictSlash(true).Path("/datasets").HandlerFunc(handler.GetAllDatasets(dc))
+	router.StrictSlash(true).Path("/datasets/{datasetID}").HandlerFunc(handler.GetDataset(dc))
 }
