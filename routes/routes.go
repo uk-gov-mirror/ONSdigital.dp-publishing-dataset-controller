df44be20cce3438ac288b2b29215897012bda3c3
Jon Dewi Jones <jondewijones@gmail.com>
2019-12-16 11:14:22 +0000

    Update names

--- routes/routes.go
+++ routes/routes.go
@@ -1,15 +1,15 @@
 package routes
 
 import (
-	"github.com/ONSdigital/dp-api-clients-go/dataset"
+	ds "github.com/ONSdigital/dp-api-clients-go/dataset"
 	zebedee "github.com/ONSdigital/dp-api-clients-go/zebedee"
 	"github.com/ONSdigital/dp-publishing-dataset-controller/config"
-	"github.com/ONSdigital/dp-publishing-dataset-controller/handler"
+	"github.com/ONSdigital/dp-publishing-dataset-controller/dataset"
 	"github.com/gorilla/mux"
 )
 
 // Init initialises routes for the service
-func Init(router *mux.Router, cfg *config.Config, dc *dataset.Client, zc *zebedee.ZebedeeClient) {
-	router.StrictSlash(true).Path("/datasets").HandlerFunc(handler.GetAllDatasets(dc))
-	router.StrictSlash(true).Path("/datasets/{datasetID}").HandlerFunc(handler.GetDataset(dc))
+func Init(router *mux.Router, cfg *config.Config, dc *ds.Client, zc *zebedee.ZebedeeClient) {
+	router.StrictSlash(true).Path("/datasets").HandlerFunc(dataset.GetAll(dc))
+	router.StrictSlash(true).Path("/datasets/{datasetID}").HandlerFunc(dataset.Get(dc))
 }
