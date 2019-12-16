53082e33b224267b1fbbae48ec22dd9747d0c3f7
Jon Dewi Jones <jondewijones@gmail.com>
2019-12-16 11:14:09 +0000

    Add graceful shutdown func

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/ONSdigital/dp-api-clients-go/dataset"
 	zebedee "github.com/ONSdigital/dp-api-clients-go/zebedee"
@@ -13,13 +17,16 @@ import (
 )
 
 func main() {
+	log.Namespace = "dp-publishing-dataset-controller"
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
 	cfg, err := config.Get()
 	if err != nil {
 		log.Event(nil, "error getting configuration", log.Error(err))
 		os.Exit(1)
 	}
-
-	log.Namespace = "dp-publishing-dataset-controller"
 	log.Event(nil, "config on startup", log.Data{"config": cfg})
 
 	router := mux.NewRouter()
@@ -31,8 +38,31 @@ func main() {
 
 	s := server.New(cfg.BindAddr, router)
 
-	if err := s.ListenAndServe(); err != nil {
-		log.Event(nil, "error starting http server", log.Error(err))
-		os.Exit(1)
+	go func() {
+		if err := s.ListenAndServe(); err != nil {
+			log.Event(nil, "error starting http server", log.Error(err))
+			os.Exit(1)
+		}
+	}()
+
+	for {
+		select {
+		case <-signals:
+			log.Event(nil, "os signal received")
+			gracefulShutdown(cfg, s)
+		}
 	}
+}
+
+func gracefulShutdown(cfg *config.Config, s *server.Server) {
+	log.Event(nil, fmt.Sprintf("shutdown with timeout: %s", cfg.GracefulShutdownTimeout))
+	ctx, cancel := context.WithTimeout(context.Background(), cfg.GracefulShutdownTimeout)
+	if err := s.Shutdown(ctx); err != nil {
+		log.Event(nil, "failed to gracefully shutdown http server", log.Error(err))
+	}
+	log.Event(nil, "graceful shutdown of http server complete", nil)
+	// TODO: health check shut down here
+	log.Event(nil, "shutdown complete", nil)
+	cancel()
+	os.Exit(1)
 }
