52e1985073b69a05f32b5700d1f90d583f6a3723
Jon Dewi Jones <jondewijones@gmail.com>
2019-12-18 07:48:18 +0000

    Add health check route

--- routes/routes.go
+++ routes/routes.go
@@ -3,13 +3,16 @@ package routes
 import (
 	ds "github.com/ONSdigital/dp-api-clients-go/dataset"
 	zebedee "github.com/ONSdigital/dp-api-clients-go/zebedee"
+	"github.com/ONSdigital/dp-healthcheck/healthcheck"
 	"github.com/ONSdigital/dp-publishing-dataset-controller/config"
 	"github.com/ONSdigital/dp-publishing-dataset-controller/dataset"
 	"github.com/gorilla/mux"
 )
 
 // Init initialises routes for the service
-func Init(router *mux.Router, cfg *config.Config, dc *ds.Client, zc *zebedee.ZebedeeClient) {
+func Init(router *mux.Router, cfg *config.Config, hc healthcheck.HealthCheck, dc *ds.Client, zc *zebedee.ZebedeeClient) {
+	router.StrictSlash(true).Path("/health").HandlerFunc(hc.Handler)
+
 	router.StrictSlash(true).Path("/datasets").HandlerFunc(dataset.GetAll(dc))
 	router.StrictSlash(true).Path("/datasets/{datasetID}").HandlerFunc(dataset.Get(dc))
 }
