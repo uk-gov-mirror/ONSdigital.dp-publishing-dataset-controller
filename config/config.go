6e27015e75509e241a8adacaf8814761b18e3299
Jon Dewi Jones <jondewijones@gmail.com>
2019-12-18 07:44:40 +0000

    Add health check configuration values

--- config/config.go
+++ config/config.go
@@ -8,10 +8,12 @@ import (
 
 // Config represents the configuration required for florence
 type Config struct {
-	BindAddr                string        `envconfig:"BIND_ADDR"`
-	ZebedeeURL              string        `envconfig:"ZEBEDEE_URL"`
-	DatasetAPIURL           string        `envconfig:"DATASET_API_URL"`
-	GracefulShutdownTimeout time.Duration `envconfig:"GRACEFUL_SHUTDOWN_TIMEOUT"`
+	BindAddr                  string        `envconfig:"BIND_ADDR"`
+	ZebedeeURL                string        `envconfig:"ZEBEDEE_URL"`
+	DatasetAPIURL             string        `envconfig:"DATASET_API_URL"`
+	GracefulShutdownTimeout   time.Duration `envconfig:"GRACEFUL_SHUTDOWN_TIMEOUT"`
+	HealthCheckInterval       time.Duration `envconfig:"HEALTHCHECK_INTERVAL"`
+	HealthCheckCritialTimeout time.Duration `envconfig:"HEALTHCHECK_CRITICAL_TIMEOUT"`
 }
 
 var cfg *Config
@@ -23,10 +25,12 @@ func Get() (*Config, error) {
 	}
 
 	cfg = &Config{
-		BindAddr:                ":24000",
-		ZebedeeURL:              "http://localhost:8082",
-		DatasetAPIURL:           "http://localhost:22000",
-		GracefulShutdownTimeout: 5 * time.Second,
+		BindAddr:                  ":24000",
+		ZebedeeURL:                "http://localhost:8082",
+		DatasetAPIURL:             "http://localhost:22000",
+		GracefulShutdownTimeout:   5 * time.Second,
+		HealthCheckInterval:       30 * time.Second,
+		HealthCheckCritialTimeout: 60 * time.Second,
 	}
 
 	return cfg, envconfig.Process("", cfg)
