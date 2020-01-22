aee22dc1b5441dfdb9acd66ff8590f1a176a4ce6
Jon Dewi Jones <jondewijones@gmail.com>
2020-01-22 17:22:37 +0000

    Remove collection info from mapped response and continue on no next item rather than returning

--- mapper/mapper.go
+++ mapper/mapper.go
@@ -5,13 +5,15 @@ import (
 	"github.com/ONSdigital/dp-publishing-dataset-controller/model"
 )
 
-func AllDatasets(datasets dataset.ModelCollection) []model.Dataset {
+func AllDatasets(datasets dataset.List) []model.Dataset {
 	var mappedDatasets []model.Dataset
 	for _, ds := range datasets.Items {
+		if &ds == nil || ds.Next == nil {
+			continue
+		}
 		mappedDatasets = append(mappedDatasets, model.Dataset{
-			ID:           ds.ID,
-			Title:        ds.Title,
-			CollectionID: ds.CollectionID,
+			ID:    ds.ID,
+			Title: ds.Next.Title,
 		})
 	}
 	return mappedDatasets
