2e253b5834a5073c86d5b83007f2130aca494260
Jon Dewi Jones <jondewijones@gmail.com>
2020-01-22 17:25:58 +0000

    Add checking for headers not set, and use http.Error() to return errors

--- dataset/get-all.go
+++ dataset/get-all.go
@@ -4,41 +4,51 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/ONSdigital/dp-api-clients-go/dataset"
+	//datasetclient "github.com/ONSdigital/dp-api-clients-go/dataset"
 	"github.com/ONSdigital/dp-api-clients-go/headers"
 	"github.com/ONSdigital/dp-publishing-dataset-controller/mapper"
 	"github.com/ONSdigital/log.go/log"
 )
 
-// GetAllDatasets returns a mapped list of all datasets
-func GetAll(dc *dataset.Client) http.HandlerFunc {
+// GetAll returns a mapped list of all datasets
+func GetAll(dc Client) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		getAll(w, req, dc)
 	}
 }
 
-func getAll(w http.ResponseWriter, req *http.Request, dc *dataset.Client) {
+func getAll(w http.ResponseWriter, req *http.Request, dc Client) {
 	ctx := req.Context()
 
 	userAccessToken, err := headers.GetUserAuthToken(req)
-	if err != nil && err != headers.ErrHeaderNotFound {
-		log.Event(ctx, "error getting user access token from header", log.Error(err))
-		w.WriteHeader(http.StatusBadRequest)
+	if err == headers.ErrHeaderNotFound {
+		log.Event(ctx, "no user access token header set", log.Error(err))
+		http.Error(w, "no user access token header set", http.StatusBadRequest)
 		return
 	}
-	collectionID, err := headers.GetCollectionID(req)
-	if err != nil && err != headers.ErrHeaderNotFound {
-		log.Event(ctx, "error getting collection ID from header", log.Error(err))
-		w.WriteHeader(http.StatusBadRequest)
+	if err != nil {
+		log.Event(ctx, "error getting user access token from header", log.Error(err))
+		http.Error(w, "error getting user access token from header", http.StatusBadRequest)
 		return
 	}
 
+	collectionID, err := headers.GetCollectionID(req)
+	if err == headers.ErrHeaderNotFound {
+		log.Event(ctx, "no collection ID header set", log.Error(err))
+		http.Error(w, "no collection ID header set", http.StatusBadRequest)
+		return
+	}
+	if err != nil {
+		log.Event(ctx, "error getting collection ID from header", log.Error(err))
+		http.Error(w, "error getting collection ID from header", http.StatusBadRequest)
+		return
+	}
 	log.Event(ctx, "calling get datasets")
 
 	datasets, err := dc.GetDatasets(ctx, userAccessToken, "", collectionID)
 	if err != nil {
-		log.Event(ctx, "error getting all datasets", log.Error(err))
-		w.WriteHeader(http.StatusInternalServerError)
+		log.Event(ctx, "error getting all datasets from dataset API", log.Error(err))
+		http.Error(w, "error getting all datasets from dataset API", http.StatusInternalServerError)
 		return
 	}
 
@@ -46,10 +56,10 @@ func getAll(w http.ResponseWriter, req *http.Request, dc *dataset.Client) {
 
 	b, err := json.Marshal(mapped)
 	if err != nil {
-		log.Event(ctx, "error marshalling json", log.Error(err))
-		w.WriteHeader(http.StatusInternalServerError)
+		log.Event(ctx, "error marshalling response to json", log.Error(err))
+		http.Error(w, "error marshalling response to json", http.StatusInternalServerError)
 		return
 	}
-
+	w.Header().Set("Content-Type", "application/json")
 	w.Write(b)
 }
