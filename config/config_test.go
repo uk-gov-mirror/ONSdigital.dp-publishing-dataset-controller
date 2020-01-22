9a5ce0093ed4d4f063793c2960be80b0b81f5025
Jon Dewi Jones <jondewijones@gmail.com>
2020-01-22 17:27:35 +0000

    Add config test

---
+++ config/config_test.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func TestSpec(t *testing.T) {
+	Convey("Given an environment with no environment variables set", t, func() {
+		cfg, err := Get()
+
+		Convey("When the config values are retrieved", func() {
+
+			Convey("Then there should be no error returned", func() {
+				So(err, ShouldBeNil)
+			})
+
+			Convey("The values should be set to the expected defaults", func() {
+				So(cfg.BindAddr, ShouldEqual, ":24000")
+				So(cfg.ZebedeeURL, ShouldEqual, "http://localhost:8082")
+				So(cfg.DatasetAPIURL, ShouldEqual, "http://localhost:22000")
+				So(cfg.GracefulShutdownTimeout, ShouldEqual, 5*time.Second)
+				So(cfg.HealthCheckInterval, ShouldEqual, 30*time.Second)
+				So(cfg.HealthCheckCritialTimeout, ShouldEqual, 60*time.Second)
+			})
+		})
+	})
+}
