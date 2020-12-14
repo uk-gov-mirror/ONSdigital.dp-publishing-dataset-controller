26a16734dc54ea5f17e07c3610f8d20b3028acae
Jon Dewi Jones <jondewijones@gmail.com>
2020-12-14 12:18:18 +0000

    Add zebedee client interface for mocking

--- dataset/client.go
+++ dataset/client.go
@@ -6,9 +6,17 @@ import (
 	datasetclient "github.com/ONSdigital/dp-api-clients-go/dataset"
 )
 
-//go:generate moq -out mocks_test.go -pkg dataset . Client
+//go:generate moq -out mocks_test.go -pkg dataset . DatasetClient ZebedeeClient
 
-type Client interface {
+type DatasetClient interface {
 	//healthcheck.Client
 	GetDatasets(ctx context.Context, userAuthToken, serviceAuthToken, collectionID string) (m datasetclient.List, err error)
+	PutDataset(ctx context.Context, userAuthToken, serviceAuthToken, collectionID, datasetID string, d datasetclient.Dataset) error
+	PutVersion(ctx context.Context, userAuthToken, serviceAuthToken, collectionID, datasetID, edition, version string, v datasetclient.Version) error
+	PutInstance(ctx context.Context, userAuthToken, serviceAuthToken, collectionID, instanceID string, i datasetclient.Instance) error
+}
+
+type ZebedeeClient interface {
+	PutDatasetInCollection(ctx context.Context, userAccessToken, collectionID, lang, datasetID, state string) error
+	PutDatasetVersionInCollection(ctx context.Context, userAccessToken, collectionID, lang, datasetID, edition, version, state string) error
 }
